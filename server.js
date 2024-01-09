ec3549d6b93e5a00e098e9bc158d9c315c7426b2
Arian <54391451+arianrexhepii@users.noreply.github.com>
2024-01-09 16:39:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ function connectToDatabase(companyId) {
   }
 
  return mysql.createConnection({
-    host: '172.16.178.110',
-    user: 'dev',
-    password: '?Abc123@',
+    host: '172.16.177.77',
+    user: 'swire',
+    password: 'Q$2szF*X*G{SU.gM',
     database: databaseName,
    port: 3306
   });
