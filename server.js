352411d7d383df23cb6b4eb6eccefaa8e63f4dad
ImerNesimi <imernesimi@outlook.com>
2024-01-19 10:36:07 +0100

    ,,

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ function connectToDatabase(companyId) {
   }
 
  return mysql.createConnection({
-    host: 'localhost',
+    host: 'http://172.16.178.110',
     user: 'swire',
     password: 'Q$2szF*X*G{SU.gM',
     database: databaseName,
